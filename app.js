7de2c3b052ab25164b67bda16532cef2f7c0a94f
amber-king <amberking@pursuit.org>
2023-07-29 09:45:04 -0400

    edited & removal of routes to get mild functionality - today route works on backend

--- app.js
+++ app.js
@@ -6,20 +6,6 @@ const cors = require("cors");
 // Configuration
 const app = express();
 
-// added cors config requirements to prevent requests going to a different domain
-// the home page and the topic page connected to the topic buttons are the two main domains the request muct got
-// const acceptOrigin = ["http://localhost:3000"];
-
-// const corsCheck = {
-//   origin: function (origin, callback) {
-//     if (acceptOrigin.includes(origin) || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-// };
-
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -29,8 +15,7 @@ app.use(express.json());
 // today quotes --v
 app.get("/api/quotes/today", async (req, res) => {
   try {
-    const response = await axios.get("https://zenquotes.io/api/random");
-    const todaysQuote = response.data[0].q;
+    const todaysQuote = response.data.quote;
     res.json({ quote: todaysQuote });
   } catch (error) {
     console.error("Error fetching today's quote:", error);
@@ -76,45 +61,45 @@ app.get("/api/quotes/happiness", async (req, res) => {
   }
 });
 
-// confidence quotes
-app.get("/api/quotes/confidence", async (req, res) => {
-  try {
-    const response = await axios.get("https://zenquotes.io/api/random");
-    const confidenceQuotes = response.data.filter((confidence) =>
-      confidence.q.toLowerCase().includes("confidence")
-    );
-    if (confidenceQuotes.length > 0) {
-      return res.json({ quotes: confidenceQuotes });
-    } else {
-      return res.status(404).json({ message: "No confidence quotes found" });
-    }
-  } catch (error) {
-    console.error("Error fetching confidence quotes:", error);
-    return res
-      .status(500)
-      .json({ message: "Failed to fetch confidence quotes." });
-  }
-});
+// // confidence quotes --v
+// app.get("/api/quotes/confidence", async (req, res) => {
+//   try {
+//     const response = await axios.get("https://zenquotes.io/api/random");
+//     const confidenceQuotes = response.data.filter((quote) =>
+//       quote.q.toLowerCase().includes("confidence")
+//     );
+//     if (confidenceQuotes.length > 0) {
+//       return res.json({ quotes: confidenceQuotes });
+//     } else {
+//       return res.status(404).json({ message: "No confidence quotes found" });
+//     }
+//   } catch (error) {
+//     console.error("Error fetching confidence quotes:", error);
+//     return res
+//       .status(500)
+//       .json({ message: "Failed to fetch confidence quotes." });
+//   }
+// });
 
-// inspiration  quotes
-app.get("/api/quotes/inspiration", async (req, res) => {
-  try {
-    const response = await axios.get("https://zenquotes.io/api/random");
-    const inspirationQuotes = response.data.filter((inspiration) =>
-      inspiration.q.toLowerCase().includes("inspiration")
-    );
-    if (inspirationQuotes.length > 0) {
-      return res.json({ quotes: inspirationQuotes });
-    } else {
-      return res.status(404).json({ message: "No inspiration quotes found" });
-    }
-  } catch (error) {
-    console.error("Error fetching inspiration quotes:", error);
-    return res
-      .status(500)
-      .json({ message: "Failed to fetch inspiration quotes." });
-  }
-});
+// // inspiration  quotes --v
+// app.get("/api/quotes/inspiration", async (req, res) => {
+//   try {
+//     const response = await axios.get("https://zenquotes.io/api/random");
+//     const inspirationQuotes = response.data.filter((inspiration) =>
+//       inspiration.q.toLowerCase().includes("inspiration")
+//     );
+//     if (inspirationQuotes.length > 0) {
+//       return res.json({ quotes: inspirationQuotes });
+//     } else {
+//       return res.status(404).json({ message: "No inspiration quotes found" });
+//     }
+//   } catch (error) {
+//     console.error("Error fetching inspiration quotes:", error);
+//     return res
+//       .status(500)
+//       .json({ message: "Failed to fetch inspiration quotes." });
+//   }
+// });
 
 // declared PORT & listening on the port
 const PORT = process.env.PORT || 5000;
