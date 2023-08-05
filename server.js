1cdcc5a04b2860e40ded1bd747fcf8b600391b58
amber-king <amberking@pursuit.org>
2023-08-05 16:40:17 -0400

    working tag & all quotes routes

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
-// Import required modules
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 
-// Create an instance of Express app
 const app = express();
-
-// Middleware to handle CORS
 app.use(cors());
 app.use(express.json());
 
-let allQuotes = [];
+const API_BASE_URL = "https://api.quotable.io";
 
-// Endpoint to fetch all quotes from the external API
+// Endpoint to fetch all quotes from the external API - http://localhost:5000/api/quotes
 app.get("/api/quotes", async (req, res) => {
   try {
-    const response = await axios.get("https://api.quotable.io/quotes");
+    const response = await axios.get(`${API_BASE_URL}/quotes`);
     const data = response.data;
     const allQuotes = data.results;
     res.json({ quotes: allQuotes });
@@ -25,27 +21,37 @@ app.get("/api/quotes", async (req, res) => {
   }
 });
 
-app.get("/api/search/quotes", (req, res) => {
+// Endpoint to fetch all available tags - http://localhost:5000/api/tags
+app.get("/api/tags", async (req, res) => {
   try {
-    const { tags } = req.query;
-
-    if (!tags) {
-      return res.status(400).json({ error: "Tag parameter is required" });
-    }
-
-    // Filter quotes that have any of the specified tags
-    const searchResults = allQuotes.filter((quote) =>
-      quote.tags.some((tag) => tags.includes(tag))
-    );
-
-    res.json({ quotes: searchResults });
+    const response = await axios.get(`${API_BASE_URL}/tags`);
+    res.json(response.data); // Send the entire response data as tags
   } catch (error) {
-    console.error("Error fetching quotes:", error);
-    res.status(500).json({ error: "Failed to fetch quotes" });
+    console.error("Error fetching tags:", error);
+    res.status(500).json({ error: "Failed to fetch tags" });
   }
 });
 
-// Define the port number and start the server
+// // Endpoint to fetch quotes by a specific tag
+// app.get("/api/search/quotes", async (req, res) => {
+//     try {
+//       const { tag } = req.query;
+  
+//       if (!tag) {
+//         return res.status(400).json({ error: "Tag parameter is required" });
+//       }
+  
+//       const response = await axios.get(`${API_BASE_URL}/search/quotes?tag=${tag}`);
+//       const data = response.data;
+//       const quotes = data.results;
+//       res.json({ quotes });
+//     } catch (error) {
+//       console.error("Error fetching quotes by tag:", error);
+//       res.status(500).json({ error: "Failed to fetch quotes by tag" });
+//     }
+//   });
+  
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
