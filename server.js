4d505456d256065e5a0f5daea4ae9de322857c50
amber-king <amberking@pursuit.org>
2023-08-07 17:25:24 -0400

    switch of filter by author - no tag filter

--- server.js
+++ server.js
@@ -21,16 +21,46 @@ app.get("/api/quotes", async (req, res) => {
   }
 });
 
+// Endpoint to fetch all quotes by author from the external API - http://localhost:5000/api/authors
+app.get("/api/authors", async (req, res) => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/authors`);
+      res.json(response.data);
+    } catch (error) {
+      console.error("Error fetching authors:", error);
+      res.status(500).json({ error: "Failed to fetch authors" });
+    }
+  });
+  
+
 // Endpoint to fetch all available tags - http://localhost:5000/api/tags
-app.get("/api/tags", async (req, res) => {
-  try {
-    const response = await axios.get(`${API_BASE_URL}/tags`);
-    res.json(response.data); // Send the entire response data as tags
-  } catch (error) {
-    console.error("Error fetching tags:", error);
-    res.status(500).json({ error: "Failed to fetch tags" });
-  }
-});
+// app.get("/api/tags", async (req, res) => {
+//   try {
+//     const response = await axios.get(`${API_BASE_URL}/tags`);
+//     res.json(response.data); // Send the entire response data as tags
+//   } catch (error) {
+//     console.error("Error fetching tags:", error);
+//     res.status(500).json({ error: "Failed to fetch tags" });
+//   }
+// });
+
+// Endpoint to fetch selected tags - http://localhost:5000/api/tag-quotes/:tagSlug
+
+// app.get("/api/tag-quotes/:tagSlug", async (req, res) => {
+//     try {
+//       const tagSlug = req.params.tagSlug;
+//       const response = await axios.get(`${API_BASE_URL}/search/quotes?tag=${tagSlug}`);
+//       const data = response.data;
+//       const tagQuotes = data.results;
+//       res.json({ quotes: tagQuotes });
+//     } catch (error) {
+//       console.error("Error fetching tag quotes:", error);
+//       res.status(500).json({ error: "Failed to fetch tag quotes" });
+//     }
+//   });
+
+
+  
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
